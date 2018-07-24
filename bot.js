5b0001158120cc7f4e91c423fabe2ea39413eb6e
Caio Torre <caiosanches@live.com>
2018-07-24 11:47:01 -0300

    Added new functionalities + bugfixes
    Added more recognizable attribute types
    Added new output for Components and DamageTypes

--- bot.js
+++ bot.js
@@ -26,171 +26,199 @@ function slowFetch(namelookup) {
 var bot = new Discord.Client({token: auth.token, autorun: true});
 
 var attributeData = {
-	'uniqueName': [
-		{
-			'want':0,
-			'alias':'Unique Name'
-		}
-	],
-	'name': [
-		{
-			'want':1,
-			'alias':'Name'
-		}
-	],
-	'description': [
-		{
-			'want':1,
-			'alias':'Description'
-		}
-	],
+	'uniqueName': 
+	{
+		'want':0,
+		'alias':'Unique Name'
+	},
+	'name': 
+	{
+		'want':1,
+		'alias':'Name'
+	},
+	'description': 
+	{
+		'want':1,
+		'alias':'Description'
+	},
 	//Warframes
-	'health': [
-		{
-			'want':1,
-			'alias':'Health'
-		}
-	],
-	'shield': [
-		{
-			'want':1,
-			'alias':'Shield'
-		}
-	],
-	'armor': [
-		{
-			'want':1,
-			'alias':'Armor'
-		}
-	],
-	'stamina': [
-		{
-			'want':0,
-			'alias':'Stamina'
-		}
-	],
-	'power': [
-		{
-			'want':1,
-			'alias':'Energy'
-		}
-	],
-	'abilities': [
-		{
-			'want':1,
-			'alias':'Abilities'
-		}
-	],
-	'type': [
-		{
-			'want':0,
-			'alias':'Type'
-		}
-	],
-	'buildPrice': [
-		{
-			'want':0,
-			'alias':'Build Price'
-		}
-	],
-	'buildTime': [
-		{
-			'want':0,
-			'alias':'Build Time'
-		}
-	],
-	'skipBuildTimePrice': [
-		{
-			'want':0,
-			'alias':'Skip Build Time Price'
-		}
-	],
-	'buildQuantity': [
-		{
-			'want':0,
-			'alias':'Build Quantity'
-		}
-	],
-	'consumeOnBuild': [
-		{
-			'want':0,
-			'alias':'Consume Blueprint on Build'
-		}
-	],
-	'components': [
-		{
-			'want':1,
-			'alias':'Components'
-		}
-	],
-	'imageName': [
-		{
-			'want':0,
-			'alias':'Image Name'
-		}
-	],
-	'category': [
-		{
-			'want':0,
-			'alias':'Category'
-		}
-	],
-	'tradable': [
-		{
-			'want':1,
-			'alias':'Tradable'
-		}
-	],
-	'patchlogs': [
-		{
-			'want':0,
-			'alias':'Patchlogs'
-		}
-	],
+	'health': 
+	{
+		'want':1,
+		'alias':'Health'
+	},
+	'shield': 
+	{
+		'want':1,
+		'alias':'Shield'
+	},
+	'armor': 
+	{
+		'want':1,
+		'alias':'Armor'
+	},
+	'stamina': 
+	{
+		'want':0,
+		'alias':'Stamina'
+	},
+	'power': 
+	{
+		'want':1,
+		'alias':'Energy'
+	},
+	'abilities': 
+	{
+		'want':1,
+		'alias':'Abilities'
+	},
+	'type': 
+	{
+		'want':0,
+		'alias':'Type'
+	},
+	'buildPrice': 
+	{
+		'want':0,
+		'alias':'Build Price'
+	},
+	'buildTime': 
+	{
+		'want':0,
+		'alias':'Build Time'
+	},
+	'skipBuildTimePrice': 
+	{
+		'want':0,
+		'alias':'Skip Build Time Price'
+	},
+	'buildQuantity': 
+	{
+		'want':0,
+		'alias':'Build Quantity'
+	},
+	'consumeOnBuild': 
+	{
+		'want':0,
+		'alias':'Consume Blueprint on Build'
+	},
+	'components': 
+	{
+		'want':1,
+		'alias':'Components'
+	},
+	'imageName': 
+	{
+		'want':0,
+		'alias':'Image Name'
+	},
+	'category': 
+	{
+		'want':0,
+		'alias':'Category'
+	},
+	'tradable': 
+	{
+		'want':1,
+		'alias':'Tradable'
+	},
+	'patchlogs': 
+	{
+		'want':0,
+		'alias':'Patchlogs'
+	},
+	'sex':
+	{
+		'want':0,
+		'alias':'Gender'
+	},
 	//Mods
-	'polarity': [
-		{
-			'want':1,
-			'alias':'Polarity'
-		}
-	],
-	'rarity': [
-		{
-			'want':0,
-			'alias':'Rarity'
-		}
-	],
-	'baseDrain': [
-		{
-			'want':0,
-			'alias':'Base Drain'
-		}
-	],
-	'fusionLimit': [
-		{
-			'want':0,
-			'alias':'Fusion Limit'
-		}
-	],
-	'drops': [
-		{
-			'want':0,
-			'alias':'Drop table'
-		}
-	],
+	'polarity': 
+	{
+		'want':1,
+		'alias':'Polarity'
+	},
+	'rarity': 
+	{
+		'want':0,
+		'alias':'Rarity'
+	},
+	'baseDrain': 
+	{
+		'want':0,
+		'alias':'Base Drain'
+	},
+	'fusionLimit': 
+	{
+		'want':0,
+		'alias':'Fusion Limit'
+	},
+	'drops': 
+	{
+		'want':0,
+		'alias':'Drop table'
+	},
 	//Primary
-	'secondsPerShot': [
-		{
-			'want':0,
-			'alias':'Seconds per shot'
-		}
-	],
-	'damagePerShot': [
-		{
-			'want':0,
-			'alias':'Damage per shot'
-		}
-	]
+	'secondsPerShot': 
+	{
+		'want':0,
+		'alias':'Seconds per shot'
+	},
+	'damagePerShot': 
+	{
+		'want':0,
+		'alias':'Damage per shot'
+	},
+	'channeling':
+	{
+		'want':0,
+		'alias':'Channeling'
+	},
+	'damage':
+	{
+		'want':0,
+		'alias':'Total Damage'
+	},
+	'damageTypes':
+	{
+		'want':1,
+		'alias':'Damage Types'
+	},
+	'disposition':
+	{
+		'want':0,
+		'alias':'Disposition'
+	},
+	'marketCost':
+	{
+		'want':0,
+		'alias':'Market Cost'
+	},
+	'polarities':
+	{
+		'want':1,
+		'alias':'Polarities'
+	},
+	'stancePolarity':
+	{
+		'want':1,
+		'alias':'Stance Polarity'
+	},
+	'tags':
+	{
+		'want':0,
+		'alias':'Tags'
+	},
+	'vaulted':
+	{
+		'want':1,
+		'alias':'Vaulted'
+	},
+	'wikiaURL':
+	{
+		'want':0,
+		'alias':'Wikia URL'
+	}
 	//INCOMPLETE
 	
 };
@@ -208,7 +236,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 			switch (args[0]) {
 				case 'track':
 					if (attributeData[args[1]] != undefined) {
-						attributeData[args[1]][0]['want'] = 1;
+						attributeData[args[1]]['want'] = 1;
 						bot.sendMessage({
 							to: channelID,
 							message: 'Now tracking ' + args[1]
@@ -222,7 +250,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					break;
 				case 'untrack':
 					if (attributeData[args[1]] != undefined) {
-						attributeData[args[1]][0]['want'] = 0;
+						attributeData[args[1]]['want'] = 0;
 						bot.sendMessage({
 							to: channelID,
 							message: 'Stopped tracking ' + args[1]
@@ -236,11 +264,63 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 					break;
 				default:
 					if (attributeData[args[0]] != undefined) {
-						var selectedItem = slowFetch(args[1]);
+						var testName = '';
+						for (var cname in args) {
+							if (cname > 0) {
+								if (cname > 1) {
+									testName += ' ';
+								}
+								testName += args[cname];
+							}
+						}
+						logger.info('Parsed name [' + testName + ']');
+						var selectedItem = slowFetch(testName);
 						if (selectedItem != null) {
+							var mess = '';
+							switch (args[0]) {
+								case 'abilities':
+									mess += '[' + testName + '] Abilities:\n';
+									var i = 1;
+									var abs = selectedItem['abilities'];
+									for (var ab in abs) {
+										mess += '\t' + i + '. ' + abs[ab]['name'] + ": " + abs[ab]['description'] + '\n';
+										i += 1;
+									}
+									break;
+									
+								case 'components':
+									mess += '[' + testName + '] Components:\n';
+									var comps = selectedItem['components'];
+									for (var component in comps) {
+										//logger.info('    Current component: ' + comps[component]['name']);
+										mess += '\t- ' + comps[component]['itemCount'] + ' x ' + comps[component]['name'] + '\n';
+										if (comps[component]['drops'] != undefined && attributeData['drops']['want'] == 1) {
+											logger.info('Drop data requested...');
+											for (var dropLoc in comps[component]['drops']) {
+												//logger.info('        Current drop location: ' + comps[component]dropLoc);
+												mess += '\t\t' + comps[component]['drops'][dropLoc]['location'] + ' [' + (100.0 * parseFloat(comps[component]['drops'][dropLoc]['chance'])) + '%]\n';
+											}
+										}
+									}
+									break;
+								
+								case 'damageTypes':
+									mess += '[' + testName + '] Damage Types:\n';
+									var damtps = selectedItem['damageTypes'];
+									for (var damage in damtps) {
+										mess += '\t- ' + damtps[damage] + ' ' + damage + '\n';
+									}
+									break;
+								default:
+									logger.info('Fetching attribute ' + args[0]);
+									mess += attributeData[args[0]]['alias'] + ': ' + selectedItem[args[0]] + '\n';
+									//logger.info('Appending ' + args[0] + ' (' + selectedItem[args[0]] + ')');
+							}
+							//logger.info('Sending message \"' + mess + '\"');
 							bot.sendMessage({
 								to: channelID,
-								message: '[' + args[1] + '] ' + attributeData[args[0]][0]['alias'] + ': ' + selectedItem[args[0]]
+								//message: '[' + args[1] + '] ' + attributeData[args[0]]['alias'] + ': ' + selectedItem[args[0]]
+								message: mess
 							});
 						} else {
 							bot.sendMessage({
@@ -279,23 +359,44 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 				for (var attribute in selectedItem) {
 					if (attributeData[attribute] != undefined) {
 						//logger.info(attribute + ': ' + attributeData[attribute][0]['want']);
-						if (attributeData[attribute][0]['want'] == 1) {
+						if (attributeData[attribute]['want'] == 1) {
 							switch (attribute) {
 								case 'abilities':
 									mess += 'Abilities:\n';
 									var i = 1;
-									var abs = selectedItem[attribute]
+									var abs = selectedItem[attribute];
 									for (var ab in abs) {
 										mess += '\t' + i + '. ' + abs[ab]['name'] + ": " + abs[ab]['description'] + '\n';
 										i += 1;
 									}
 									break;
 									
+								case 'components':
+									mess += 'Components:\n';
+									var comps = selectedItem[attribute];
+									for (var component in comps) {
+										//logger.info('    Current component: ' + comps[component]['name']);
+										mess += '\t- ' + comps[component]['itemCount'] + ' x ' + comps[component]['name'] + '\n';
+										if (comps[component]['drops'] != undefined && attributeData['drops']['want'] == 1) {
+											for (var dropLoc in comps[component]['drops']) {
+												//logger.info('        Current drop location: ' + comps[component]dropLoc);
+												mess += '\t\t' + comps[component]['drops'][dropLoc]['location'] + ' [' + (100.0 * parseFloat(comps[component]['drops'][dropLoc]['chance'])) + '%]\n';
+											}
+										}
+									}
+									break;
+								
+								case 'damageTypes':
+									mess += 'Damage Types:\n';
+									var damtps = selectedItem['damageTypes'];
+									for (var damage in damtps) {
+										mess += '\t- ' + damtps[damage] + ' ' + damage + '\n';
+									}
+									break;
+
 								default:
-									//if (attribute != 'uniqueName' && attribute != 'imageName' && attribute != 'drops' && attribute != 'patchlogs') {
-									
-										mess += attributeData[attribute][0]['alias'] + ': ' + selectedItem[attribute] + '\n';
-										logger.info('Appending ' + attribute + ' (' + selectedItem[attribute] + ')');
+									mess += attributeData[attribute]['alias'] + ': ' + selectedItem[attribute] + '\n';
+									logger.info('Appending ' + attribute + ' (' + selectedItem[attribute] + ')');
 							}
 						} else {
 							logger.info('Unwanted attribute ' + attribute);
